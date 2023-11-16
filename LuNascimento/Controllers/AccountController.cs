aadbb03c24183a2c3e77ee0acece0ca0cfa33ece
gynna2006 <giovannacolonisio@gmail.com>
2023-11-16 11:55:29 -0300

    login adm alteracao pagina inicial

---
+++ LuNascimento/Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LuNascimento.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AccountController(
+        ILogger<AccountController> logger,
+        SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager)
+    {
+        _logger = logger;
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
+
+    [HttpGet]
+    public IActionResult Login(string ReturnUrl)
+    {
+        LoginVM login = new(){
+            UrlRetorno = ReturnUrl ?? Url.Content("~/")
+        };
+        return View(login);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (ModelState.IsValid)
+        {
+            // Verifica o Login
+            string userName = login.Email;
+            // Verificando se o login é por email
+            if (IsValidEmail(login.Email))
+            {
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if (user != null)
+                    userName = user.UserName;
+            }
+            // Login é só por UserName
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Senha, login.Lembrar, lockoutOnFailure: true
+            );
+            
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
+                return LocalRedirect(login.UrlRetorno);
+            }
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário {login.Email} está bloqueado");
+                return RedirectToAction("Lockout");
+            }
+            ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos!!");
+        }
+        return View(login);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+        _logger.LogInformation($"Usuário {ClaimTypes.Email} fez logoff");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+
+    private static bool IsValidEmail(string email)
+    {
+        try
+        {
+            MailAddress mail = new(email);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    }
---
+++ LuNascimento/ViewModels/LoginVM.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+namespace LuNascimento.ViewModels;
+
+public class LoginVM
+{
+    [Display(Name = "Email ou Nome de Usuário", Prompt = "Informe seu Email ou Nome de Usuário")]
+    [Required(ErrorMessage = "Por favor, informe seu email ou nome de usuário")]
+    public string Email { get; set; }
+    
+    [Display(Name = "Senha de Acesso", Prompt = "Informe sua Senha de Acesso")]
+    [Required(ErrorMessage = "Por favor, informe sua senha de acesso")]
+    [DataType(DataType.Password)]
+    public string Senha { get; set; }
+    
+    [Display(Name = "Manter Conectado?")]
+    public bool Lembrar { get; set; } = false;
+    
+    public string UrlRetorno { get; set; }
+}
